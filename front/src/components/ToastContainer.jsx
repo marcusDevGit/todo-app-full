03e08c90dfd0136198731e37f6f6f856ad2716d7
marcusDevGit <marcus_psc@hotmail.com>
2025-12-21 11:54:10 -0300

    fix: corrigir typo no posicionamento do ToastContainer
    - Alterar 'buttom-4' para 'bottom-4' para exibir notificações corretamente

--- front/src/components/ToastContainer.jsx
+++ front/src/components/ToastContainer.jsx
@@ -6,7 +6,7 @@ const ToastContainer = () => {
   const { toasts, removeToast } = useContext(ToastContext);
 
   return (
-    <div className="fixed buttom-4 right-4 z-50 space-y-2">
+    <div className="fixed top-4 right-4 z-50 space-y-2">
       {toasts.map((toast) => (
         <Toast
           key={toast.id}

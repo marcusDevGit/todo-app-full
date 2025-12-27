0c5b054a0aa2c70f2b01f0f1224a38227ca55f4c
marcusDevGit <marcus_psc@hotmail.com>
2025-12-26 21:59:36 -0300

    fix: corrigir nome do método createTag em taskService
    - Renomear createTags para createTag (singular)
    - Adicionar método getTags e createTag ao serviço

--- front/src/services/taskService.js
+++ front/src/services/taskService.js
@@ -2,12 +2,11 @@ import api from "./api";
 
 export const taskService = {
   getTasks: () => api.get("/tasks"),
-
   createTask: (data) => api.post("/tasks", data),
-
   updateTask: (id, data) => api.put(`/tasks/${id}`, data),
-
   deleteTask: (id) => api.delete(`/tasks/${id}`),
   getSubtasks: (id) => api.get(`/tasks/${id}/subtasks`),
   createSubtask: (id, data) => api.post(`/tasks/${id}/subtasks`, data),
+  getTags: () => api.get("/tags"),
+  createTag: (data) => api.post("/tags", data),
 };

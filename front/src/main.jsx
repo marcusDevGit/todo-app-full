fbfc03e8f22d62e3a37a45dc592f5a2027433bb5
marcusDevGit <marcus_psc@hotmail.com>
2025-12-29 16:43:28 -0300

    refactor(frontend): replace custom toast with react-toastify
    - Remove custom Toast, ToastContainer, ToastContext
    - Integrate react-toastify for notifications
    - Simplify toast implementation

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,7 +4,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
-import { ToastProvider } from "./context/ToastContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
